973b574a623f52e8df613ed37c352cb1f3b94dba
Geronimo <concha.geronimojose@iesconcepcion.edu.ar>
2023-10-11 19:06:43 -0300

    Terminando el proyecto

--- app.js
+++ app.js
@@ -1,4 +1,44 @@
 const nmrRandom =()=>{
-    return Math.floor(Math.random()*6+1)
+    return Math.floor(Math.random()*6+1);
 }
+
+const lanzarDosDados = ()=>{
+    let dados = [nmrRandom(),nmrRandom()];
+    return dados;
+}
+
+const contarRepeticiones = (lista)=>{
+    return lista.reduce((c,e)=>{
+        c[e] = (c[e] || 0)+1;
+        return c
+    },{})
+}
+
+const lanzarDados = (cantidad)=>{
+    const objeto = {
+        suma: [],
+        lazamientos:[],
+        apariciones:[]
+    }
+
+    for(let i=0;i<cantidad;i++){
+        let dados = lanzarDosDados()
+        objeto.suma.push(dados[0]+dados[1])
+        objeto.lazamientos.push(dados)
+    }
+
+    objeto.apariciones = contarRepeticiones(objeto.suma)
+    return objeto
+}
+
+const generarTabla = (cantidad)=>{
+    const objeto = lanzarDados(cantidad)
+
+    console.log(objeto.apariciones)
+    console.table(objeto)
+}
+
+let nmr = parseInt(prompt('Ingrese la cantidad de lanzamientos'))
+
+generarTabla(nmr)
 
